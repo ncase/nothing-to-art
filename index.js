1944c3a2e23a31bcc69efe14d9a4c7eb8e667fb2
Nicklaus Liow <nicklaus.liow@gmail.com>
2014-03-04 14:35:56 -0800

    alright, send to heroku

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ domain.on('error', function(err){
     console.log('Application Error: ' + err.message);
 });
 domain.run(function(){
-    require('./src/app')();
+
+	var express = require('express');
+    var app = express();
+    app.use('/', express.static('./static'));
+    var port = process.env.PORT || 1337;
+    app.listen(port);
+    console.log("App running on http://localhost:"+port);
+
 });
